45256aa2d40e35e313b1e0419b1998549a08f096
Gordenguan <gordenguan122@gmail.com>
2023-05-18 16:26:10 -0600

    tail

---
+++ tail.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+let tail = function(arr) {
+  let tailArr = [];
+  for (let i = 1; i < arr.length; i++) {
+    tailArr.push(arr[i]);
+  }
+  return tailArr;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
