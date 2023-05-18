b42a2cbcdf84ad380538bd6fb33d217c811405df
Gordenguan <gordenguan122@gmail.com>
2023-05-18 15:58:37 -0600

    2files

--- head.js
+++ head.js
@@ -6,5 +6,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+let head = function(arr) {
+  if (arr.length === 0) {
+    return undefined;
+  }
+  return arr[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
