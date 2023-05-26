3289c4b2d62b9879c4fd0b913f626ac4206afe7c
Gordenguan <gordenguan122@gmail.com>
2023-05-26 00:11:48 -0600

    map

---
+++ map.js
@@ -0,0 +1,22 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const callback = item => item[0];
+
+assertArraysEqual(map(words, callback)[1], 'c');
+assertArraysEqual(map(words, callback)[3], 'm');
