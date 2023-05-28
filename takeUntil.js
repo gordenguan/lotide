7ca700a73debf2a438432986b9130daeeb31b98b
Gordenguan <gordenguan122@gmail.com>
2023-05-27 18:00:39 -0600

    takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,29 @@
+const assertArraysEqual = function(arr1, arr2) {
+  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let answer = [];
+  for (let item of array) {
+    if (!callback(item)) {
+      answer.push(item);
+    } else {
+      break;
+    }
+  }
+  return answer;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
