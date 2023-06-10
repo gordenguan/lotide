d994ba5d01b7ebe5bbcddbfd9d6104a1f1f5a78c
Gordenguan <gordenguan122@gmail.com>
2023-06-09 20:42:24 -0600

    refactor assertArraysEqual and test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -6,17 +6,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-const eqArrays = function(arr1, arr2) {
-  if (JSON.stringify(arr1) === JSON.stringify(arr2)) {
-    return (true);
-  } else {
-    return (false);
-  }
-};
-
-
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
